d74d6420b5571aafe5dfe22da0af87a38979a0ea
erkerc <erkan.ercan@gmail.com>
2025-06-23 12:21:32 +0000

    added bert

--- examples/ui/gradio/gradio-rag-milvus-vllm-openai/app.py
+++ examples/ui/gradio/gradio-rag-milvus-vllm-openai/app.py
@@ -48,15 +48,14 @@ PROMPT_FILE = os.getenv('PROMPT_FILE', 'default_prompt.txt')
 MAX_RETRIEVED_DOCS = int(os.getenv('MAX_RETRIEVED_DOCS', 4))
 SCORE_THRESHOLD = float(os.getenv('SCORE_THRESHOLD', 0.99))
 
-s3_endpoint_url = os.environ.get('AWS_S3_ENDPOINT')
-bucket_name = os.environ.get('AWS_S3_BUCKET')
-access_key = os.environ.get('AWS_ACCESS_KEY_ID')
-secret_key = os.environ.get('AWS_SECRET_ACCESS_KEY')
-s3_model_path = os.environ.get('S3_EMBEDDING_MODEL_PATH')
+# s3_endpoint_url = os.environ.get('AWS_S3_ENDPOINT')
+# bucket_name = os.environ.get('AWS_S3_BUCKET')
+# access_key = os.environ.get('AWS_ACCESS_KEY_ID')
+# secret_key = os.environ.get('AWS_SECRET_ACCESS_KEY')
+# s3_model_path = os.environ.get('S3_EMBEDDING_MODEL_PATH')
 
 #########################
 #### TEST VARIABLES
-
 
 INFERENCE_SERVER_URL = os.getenv('INFERENCE_SERVER_URL','http://llama-32-cpu-predictor.llm-test.svc.cluster.local:8080/v1')
 MODEL_NAME = os.getenv('MODEL_NAME', 'llama-32-cpu')
@@ -71,7 +70,9 @@ s3_endpoint_url = os.environ.get('AWS_S3_ENDPOINT','http://s3.openshift-storage.
 bucket_name = os.environ.get('AWS_S3_BUCKET','http://s3.openshift-storage.svc:80')
 access_key = os.environ.get('AWS_ACCESS_KEY_ID','0HF4Pa9HJHF1uWBaHEUU')
 secret_key = os.environ.get('AWS_SECRET_ACCESS_KEY','EQhqpLlxS2KyL+0O+YwgGXW29qClr3PU+T2AsHja')
-s3_model_path = os.environ.get('S3_EMBEDDING_MODEL_PATH','/nomic-ai/nomic-embed-text-v1/')
+s3_model_path = os.environ.get('S3_EMBEDDING_MODEL_PATH','/nomic-ai/')
+local_model_dir = os.environ.get('LOCAL_MODEL_DIR','./downloaded_models/nomic-ai')
+
 
 
 
@@ -206,7 +207,7 @@ def stream(input_text, selected_collection) -> Generator:
 
     
 s3_model_path = os.environ.get('S3_EMBEDDING_MODEL_PATH')
-local_model_dir = './downloaded_models/nomic-embed-text-v1'
+local_model_dir = './downloaded_models/nomic-ai'
 
 s3 = s3fs.S3FileSystem(
     client_kwargs={'endpoint_url': s3_endpoint_url},
